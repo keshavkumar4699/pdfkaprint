17c9da178fc60f9e458f0f44f8661121d2afeee1
keshavkumar4699 <kumar.keshav4699@gmail.com>
2024-12-30 12:54:00 +0530

    site name changed in config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
   appDescription:
     "The one stop solution for all your pdf needs.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "pdfkaprint.in",
+  domainName: "pdfkaprint.site",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (resend.supportEmail) otherwise customer support won't work.
     id: "",
